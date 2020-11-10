f20e0101e46405a8a698c422b4b4b576b5c24e4b
Tiago-devop <tiagoramosvasconcelos.work@gmail.com>
2020-11-10 14:11:50 -0300

    renderizando elementos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import element from './ex002'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <h1>Hello world!</h1>
-  </React.StrictMode>,
+  element,
   document.getElementById('root')
 );
